be63dbf6dcb78bb734eacbd46a5ccbbdc96d1fd7
achal <achalsayee2@gmail.com>
2023-07-27 15:09:27 +0530

    code

---
+++ index.js
@@ -0,0 +1,16 @@
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+var peerExpress = require('express');
+var peerApp = peerExpress();
+const cors = require("cors");
+var peerServer = require('http').createServer(peerApp);
+var options = { debug: true }
+peerApp.use(cors()); 
+
+peerApp.use('/peerjs', ExpressPeerServer(peerServer, options));
+// Mount the Peer server as middleware
+
+// Start the Express server
+const PORT = 3009;
+peerServer.listen(PORT, () => {
+  console.log(`Peer server listening on port ${PORT}`);
+});
